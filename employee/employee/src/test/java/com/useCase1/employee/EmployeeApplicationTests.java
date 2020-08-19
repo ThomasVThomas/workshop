f8cf99f2566ec891e2d94d0ae59fee0ed660e3f9
ThomasVThomas <thomasplkd@gmail.com>
2020-08-19 16:42:45 +0530

    first commit to branch

---
+++ employee/employee/src/test/java/com/useCase1/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.useCase1.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
